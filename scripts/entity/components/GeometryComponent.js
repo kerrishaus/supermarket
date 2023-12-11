02de9d3267c0cc1273c53efe25fd5660ad4b86a9
Kenny McCormick <kennyrmccormick+github@gmail.com>
2023-12-11 14:57:03 -0600

    create GeometryComponent

---
+++ scripts/entity/components/GeometryComponent.js
@@ -0,0 +1,29 @@
+import { Mesh, Box3 } from "https://kerrishaus.com/assets/threejs/build/three.module.js";
+
+import { EntityComponent } from "./EntityComponent.js";
+
+export class GoemetryComponent extends EntityComponent
+{
+    constructor(geometry, material)
+    {
+        super();
+
+        this.mesh = new Mesh(geometry, material);
+
+        this.castShadow = true;
+        this.receiveShadow = true;
+
+        this.geometry.computeBoundingBox();
+
+        this.box = new Box3();
+    }
+    
+    update(deltaTime)
+    {
+        super.update(deltaTime);
+
+        this.mesh.position.copy(this.parentEntity.position);
+
+        this.box.copy(this.mesh.geometry.boundingBox).applyMatrix4(this.mesh.matrixWorld);
+    }
+};
