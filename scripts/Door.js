c144cdfdd7b2c243e7cd9a68c752a506e78258a1
Kenny McCormick <kennyrmccormick+github@gmail.com>
2023-12-11 14:51:51 -0600

    add door objects

--- scripts/Door.js
+++ scripts/Door.js
@@ -19,13 +19,16 @@ export class Door extends Triggerable
         this.material.transparent = true;
         this.material.opacity = 0;
         
-        this.leftDoor = new Entity(2, 1, 4, 0x00d1e8);
+        this.leftDoor = new Entity();
         this.leftDoor.addComponent(new CarryableComponent);
         this.leftDoor.position.copy(position);
         this.leftDoor.position.x -= 1;
         this.leftDoor.position.y -= 0.01;
         this.leftDoor.getComponent("CarryableComponent").setTarget(this.leftDoor.position, new Vector3(0, 0, 0.5));
         
+        // main door
+        this.leftDoor.add(createCube(new Vector3(2, 1, 4), new Vector3(-0.9, -0.55, 0), 0x00d1e8));
+
         this.leftDoor.add(createCube(new Vector3(0.2, 0.1, 4), new Vector3(-0.9, -0.55, 0), 0x919191));
         this.leftDoor.add(createCube(new Vector3(0.2, 0.1, 4), new Vector3(0.9, -0.55, 0), 0x919191));
         
@@ -33,12 +36,15 @@ export class Door extends Triggerable
         this.leftDoor.add(createCube(new Vector3(1.6, 0.1, 0.2), new Vector3(0, -0.55, 0.1), 0x919191));
         this.leftDoor.add(createCube(new Vector3(1.6, 0.1, 0.2), new Vector3(0, -0.55, -1.4), 0x919191));
         
-        this.rightDoor = new Entity(2, 1, 4, 0x00d1e8)
+        this.rightDoor = new Entity();
         this.rightDoor.addComponent(new CarryableComponent);
         this.rightDoor.position.copy(position);
         this.rightDoor.position.x += 1;
         this.rightDoor.position.y -= 0.01;
         this.rightDoor.getComponent("CarryableComponent").setTarget(this.rightDoor.position, new Vector3(0, 0, 0.5));
+
+        // main door
+        this.leftDoor.add(createCube(new Vector3(2, 1, 4), new Vector3(-0.9, -0.55, 0), 0x00d1e8));
 
         this.rightDoor.add(createCube(new Vector3(0.2, 0.1, 4), new Vector3(-0.9, -0.55, 0), 0x919191));
         this.rightDoor.add(createCube(new Vector3(0.2, 0.1, 4), new Vector3(0.9, -0.55, 0), 0x919191));
