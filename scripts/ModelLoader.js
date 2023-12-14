f48590714b3952e52e6c3e622789f5e997999b9c
Kenny McCormick <kennyrmccormick+github@gmail.com>
2023-12-14 17:21:50 -0600

    fix dark models

--- scripts/ModelLoader.js
+++ scripts/ModelLoader.js
@@ -18,6 +18,16 @@ export async function loadModel(modelName)
 
     const model = await loader.loadAsync(`models/${modelName}.glb`);
 
+    console.log(model);
+
+    // TODO: there is probably a better solution to this
+    // all models currently need a metalness of 0 or they
+    // are rendered with very little lighting
+    model.scene.traverse((object) => {
+        if ('material' in object)
+            object.material.metalness = 0;
+    });
+
     modelCache.set(modelName, model.scene);
 
     console.log("loaded model " + modelName);
