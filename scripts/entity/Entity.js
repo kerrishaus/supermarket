955ba6dc1d4d7b9cad5f7ee651898d5a65ab7bf9
Kenny McCormick <kennyrkun@users.noreply.github.com>
2025-05-13 13:14:14 -0500

    add Serialise/Deserialise functions to Entity

--- scripts/entity/Entity.js
+++ scripts/entity/Entity.js
@@ -65,4 +65,55 @@ export class Entity extends Object3D
     {
         return this.components.get(componentType) ?? null;
     }
+    
+    serialise()
+    {
+        const data = {
+            position: {
+                x: this.position.x,
+                y: this.position.y,
+                z: this.position.z,
+            },
+            rotation: {
+                x: this.rotation.x,
+                y: this.rotation.y,
+                z: this.rotation.z
+            }
+        };
+        
+        if ('name' in this)
+            data.type = this.name;
+        
+        if (this.components.size > 0)
+        {
+            data.components = {};
+            
+            this.components.forEach((component, componentType, map) => {
+                data.components[componentType] = component.serialise();
+            });
+        }
+        
+        return data;
+    }
+    
+    deserialise(data)
+    {
+        if ("position" in data)
+        {
+            this.position.x = data.position.x;
+            this.position.y = data.position.y;
+            this.position.z = data.position.z;
+        }
+         
+        if ("rotation" in data)   
+        {
+            this.rotation.x = data.rotation.x;
+            this.rotation.y = data.rotation.y;
+            this.rotation.z = data.rotation.z;
+        }
+        
+        if ("components" in data)
+            for (const [componentName, componentData] of Object.entries(data.components))
+                this.getComponent(componentName).deserialise(componentData);
+    }
 }
