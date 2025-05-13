307e2748bda9bc9be4bfa029ad0a3621be9fb6c6
Kenny McCormick <kennyrkun@users.noreply.github.com>
2025-05-13 13:15:13 -0500

    Create ItemUtility.js

---
+++ scripts/ItemUtility.js
@@ -0,0 +1,26 @@
+import { Tomato  } from "./items/Tomato.js";
+import { SodaCan } from "./items/SodaCan.js";
+import { Ketchup } from "./items/Ketchup.js";
+
+export function instantiateItem(itemData)
+{
+    let newItem = null;
+
+    switch (itemData.type)
+    {
+        case "tomato":
+            newItem = new Tomato(player.position);
+            break;
+        case "sodaCan":
+            newItem = new SodaCan(player.position);
+            break;
+        case "ketchup":
+            newItem = new Ketchup(player.position);
+            break;
+        default:
+            console.log("Unknown item: " + itemData);
+            return null;
+    }
+
+    return newItem;
+}
