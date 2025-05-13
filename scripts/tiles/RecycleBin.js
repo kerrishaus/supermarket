500a25b088a4279ca659568e4ee0e28c43f8a567
Kenny McCormick <kennyrkun@users.noreply.github.com>
2025-05-13 13:23:15 -0500

    simplify RecycleBin to not use ContainerComponent
    it doesn't need it

--- scripts/tiles/RecycleBin.js
+++ scripts/tiles/RecycleBin.js
@@ -4,7 +4,6 @@ import { Player } from "../Player.js";
 
 import { Entity             } from "../entity/Entity.js";
 import { TriggerComponent   } from "../entity/components/TriggerComponent.js";
-import { ContainerComponent } from "../entity/components/ContainerComponent.js";
 import { GeometryComponent  } from "../entity/components/GeometryComponent.js";
 
 export class RecycleBin extends Entity
@@ -12,19 +11,18 @@ export class RecycleBin extends Entity
     constructor()
     {
         super();
-
+        
         this.name = "recycleBin";
-
+        
         this.addComponent(new TriggerComponent);
-
-        const recycleBinContainer = this.addComponent(new ContainerComponent);
-        recycleBinContainer.name = "Recycle Bin";
-
+        
         this.addComponent(new GeometryComponent(
             new BoxGeometry(1.5, 1.5, 1), 
             new MeshStandardMaterial({ color: 0xff0000 })
         )).mesh.position.z -= 0.5;
-
+        
+        this.itemBuffer = [];
+        
         this.timeSinceLastRecycle = 0;
         this.recycleInterval = 0.2;
     }
@@ -35,10 +33,10 @@ export class RecycleBin extends Entity
         
         this.timeSinceLastRecycle += deltaTime;
         
-        for (const item of this.getComponent("ContainerComponent").carriedItems)
+        for (const item of this.itemBuffer)
             if (item.getComponent("CarryableComponent").elapsedTime > item.getComponent("CarryableComponent").moveTime)
             {
-                this.getComponent("ContainerComponent").carriedItems.splice(this.getComponent("ContainerComponent").carriedItems.indexOf(item), 1);
+                this.itemBuffer.splice(this.itemBuffer.indexOf(item), 1);
                 item.destructor();
             }
     }
@@ -52,7 +50,9 @@ export class RecycleBin extends Entity
             
         const item = holderContainer.carriedItems[holderContainer.carriedItems.length - 1]
         holderContainer.carriedItems.splice(holderContainer.carriedItems.indexOf(holder), 1);
-        this.getComponent("ContainerComponent").carriedItems.push(item);
+        
+        this.itemBuffer.push(item);
+        
         item.getComponent("CarryableComponent").setTarget(this.position, new Vector3(0, 0, -1));
     }
     
